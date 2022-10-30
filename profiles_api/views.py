b93226f59740c887af3fbe4b7e19bf68aaf9bc68
jeton <jetonstojku@hotmail.com>
2022-10-30 23:15:16 +0100

    Invoice, invoice items, total invoice

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -145,10 +145,6 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Product.objects.all()
     permission_classes = (IsAuthenticated,)
 
-    def perform_create(self, serializer):
-        """Sets the user profile to the logged in user"""
-        serializer.save(user_profile=self.request.user)
-
 
 class InvoiceViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
@@ -165,4 +161,17 @@ class InvoiceItemViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
-        serializer.save(price=self.product.price)
+        if serializer.is_valid():
+            item = serializer.validated_data
+            product = item.get('product')
+            product.quantity -= item.get('quantity')
+            if product.quantity >= 0:
+                product.save()
+                serializer.save(price=product.price)
+
+    def perform_update(self, serializer):
+        item = serializer.validated_data
+        product = item.get('product')
+        product.quantity -= serializer.validated_data.get('quantity')
+        product.save()
+        serializer.save(price=product.price)
